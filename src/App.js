20e0bff7d9abbc59cffc69bebf24642d458b95ed
Harsh <harsh.makadia@marutitech.com>
2019-03-20 10:43:30 +0530

    feat: node click tracking

--- src/App.js
+++ src/App.js
@@ -21,52 +21,65 @@ const myTreeData = [
         children: [
             {
                 name: "Level 2: A",
-                attributes: {
-                    "Node": "Child"
-                },
                 children: [
                     {
                         name: "Sub Child 1",
-                        attributes: {
-                            "Node": "Sub Child"
-                        }
                     },
                     {
                         name: "Sub Child 2",
-                        attributes: {
-                            "Node": "Sub Child"
-                        }
                     },
                     {
                         name: "Sub Child 3",
-                        attributes: {
-                            "Node": "Sub Child"
-                        },
                         children: [
                             {
-                                name: "Sub Sub Child 1",
+                                name: "Super Child 1",
                             },
                             {
-                                name: "Sub Sub Child 2",
+                                name: "Super Child 2",
+                                children: [
+                                    {
+                                        name: "New Child 1",
+                                    },
+                                    {
+                                        name: "New Child 2",
+                                    },
+                                    {
+                                        name: "New Child 3",
+                                    },
+                                    {
+                                        name: "New Child 4",
+                                    }
+                                ]
                             },
                             {
-                                name: "Sub Sub Child 3",
+                                name: "Super Child 3",
                             },
                             {
-                                name: "Sub Sub Child 4",
+                                name: "Super Child 4",
                             }
                         ]
                     },
                     {
                         name: "Sub Child 4",
-                        attributes: {
-                            "Node": "Sub Child"
-                        }
                     }
                 ]
             },
             {
-                name: "Level 2: B"
+                name: "Level 2: B",
+                children: [
+                    {
+                        name: "Me Child 1",
+                    },
+                    {
+                        name: "Me Child 2",
+                    },
+                    {
+                        name: "Me Child 3",
+                    },
+                    {
+                        name: "Me Child 4",
+                    }
+                ]
             }
         ]
     }
@@ -75,16 +88,21 @@ const myTreeData = [
 class App extends React.Component {
     constructor() {
         super();
+        this.state = {
+            name: "Nothing"
+        }
         this.trackData = this.trackData.bind(this);
     }
 
     trackData(nodeData, evt) {
         console.log({ nodeData, evt });
+        this.setState({name: nodeData.name});
     }
 
     render() {
         return (
             <div id="treeWrapper" style={{ width: "100%", height: "100vh" }}>
+                You Clicked - {this.state.name}
                 <Tree
                     collapsible={true}
                     onClick={(nodeData, evt) => this.trackData(nodeData, evt)}
